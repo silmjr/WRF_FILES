27d4628e9a76c96061091ec5da170320de12bff8
Silvano Junior <silvanomoreirajunior@gmail.com>
2017-08-18 12:27:16 -0300

    echo

--- config.sh
+++ config.sh
@@ -4,14 +4,15 @@ if (git status --porcelain);then
     echo "IT IS CLEAN"
 else
 
-	file="file.txt"
-	if [ -f "$file" ]
-	then
+	file="fileOld.txt"
+	if [ -f "$file" ] then
 		rm fileOld.txt
 	fi
 	mv file.txt fileOld.txt
 	ls -1 >> file.txt
-	diff fileOld.txt file.txt >> dif.txt 
+	diff fileOld.txt file.txt >> dif.txt
+	git add -A 
+	git commit -m " "
 fi
 
 #while read line 
