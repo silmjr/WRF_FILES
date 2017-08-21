5c2a08b216264618310bc734eb84921fe5dc6bd7
Silvano Junior <silvanomoreirajunior@gmail.com>
2017-08-21 14:40:29 -0300

    Mon, 21 Aug 2017 14:40:29 -0300

--- config.sh
+++ config.sh
@@ -1,21 +1,46 @@
 #! /bin/bash 
-
-if (git status --porcelain);then
+#Verifica se houve alteracão no repositório
+if !(git status --porcelain);then
     echo "IT IS CLEAN"
+    ls -1 >> file.txt 
 else
-
+	#caso haja verifica se o fileOld existe, se existe deleta	
 	file="fileOld.txt"
-	if [ -f "$file" ] then
+	if [ -f "$file" ]; then
+		echo "FileOld"
 		rm fileOld.txt
 	fi
+	#Verifica se existe o arquivo dif.txt, se existir deleta
+	file="dif.txt"
+	if [ -f "$file" ]; then
+		echo "Diff"
+		rm dif.txt
+	fi
+	#modifica o nome do arquivo file, para fileOld
 	mv file.txt fileOld.txt
+	#guarda as novas informacões no file
 	ls -1 >> file.txt
-	diff fileOld.txt file.txt >> dif.txt
+	#Verifica as diferenćas entre file e fileOld 
+	diff file.txt fileOld.txt  >> dif.txt
+	#Deleta informacões desnecessárias do arquivo dif 
+	sed -i 's/> //g' dif.txt
+	sed -i '/^file/d' dif.txt
+	sed -i '/^dif/d' dif.txt
+	sed -i '/^[[:digit:]]/d' dif.txt
+	sed -i '/^$/d' dif.txt
+	#Adiciona as modificacões ao repositório
 	git add -A 
-	git commit -m "   "
+	git commit -m "$(date -R)"
+	git push -u origin master 
+	
+	while read linha 
+	do
+	link = "https://raw.githubusercontent.com/silmjr/WRF_FILES/master/"
+	link = $link$linha 
+		#Insert aqui... 
+		echo $linha
+		echo $link 
+	done < dif.txt
 fi
 
-#while read line 
-#do  
-#   echo -e "$linen" 
-#done < file.txt
+
